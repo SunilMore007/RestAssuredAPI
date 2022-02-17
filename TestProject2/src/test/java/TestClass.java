f7543d498dcc64dff4575f54201b5aff145a5cd4
sunil.more <sunil.more@BLR1-LHP-N71512.synapse.com>
2022-02-17 11:05:55 +0530

    Test project 2

---
+++ TestProject2/src/test/java/TestClass.java
@@ -0,0 +1,13 @@
+package test.java;
+
+public class TestClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		
+		System.out.println("First Class");
+	}
+
+}
